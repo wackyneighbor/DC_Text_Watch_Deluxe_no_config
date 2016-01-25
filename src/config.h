350a6a7496e76ac5237b287edf9340cbc2d6f809
wackyneighbor <wackyneighbor@users.noreply.github.com>
2016-01-24 21:22:38 -0800

    first push from CloudPebble

---
+++ src/config.h
@@ -0,0 +1,23 @@
+// Single digit handling:
+// Show "Oh" on line above single digits?
+#define TimeRenderOh         false
+//
+// Prefix single digits with O'?
+#define TimeRenderO          true
+//
+// (Don't set both of these to true, one or neither)
+
+
+#define BACKGROUND_COLOR GColorBlack
+#define TEXT_LINE_1_COLOR GColorWhite
+#define TEXT_LINE_2_COLOR GColorWhite
+#define TEXT_LINE_3_COLOR GColorWhite
+#define TEXT_DAY_COLOR GColorWhite
+#define TEXT_DATE_COLOR GColorWhite
+
+// Choose a color that contrasts with the date line, as the time
+// indicatore will pass behind for several hours around midnight
+#define TIME_INDICATOR_COLOR GColorLimerick
+
+#define SUNRISE_INDICATOR_COLOR GColorLightGray
+#define SUNSET_INDICATOR_COLOR GColorLightGray
